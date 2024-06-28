9ae640535bf6cce564c3d4922445fc18f96f1f4a
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-06-28 14:27:58 +0530

    Implementatin for passing `testAdd_EmptyString_ReturnsZero` testcase

---
+++ app/src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        return 0; // Minimal implementation to pass `Add_EmptyString_ReturnsZero` test
+    }
+}
