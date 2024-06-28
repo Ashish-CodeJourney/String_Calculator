08a8a0a64296b3bde74ea38881c04e326485fdfd
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-06-28 14:54:29 +0530

    Implementatin for passing `testAdd_CustomDelimiter_ReturnsSum` testcase

--- app/src/main/java/stringcalculator/StringCalculator.java
+++ app/src/main/java/stringcalculator/StringCalculator.java
@@ -7,12 +7,23 @@ public class StringCalculator {
             return 0;
         }
 
+        String delimiter = ",|\n"; // comma and new line are default delimiters
+        String numbersWithoutDelimiter = numbers;
+
+        // implementation to pass `testAdd_CustomDelimiter_ReturnsSum` test
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbersWithoutDelimiter = numbers.substring(delimiterIndex + 1);
+        }
+
         // implementation to pass `testAdd_SingleNumber_ReturnsNumber` test
         // Split numbers by comma or new line and calculate sum for `testAdd_NewLinesBetweenNumbers_ReturnsSum` test
-        String[] nums = numbers.split("[,\n]");
+        String[] nums = numbersWithoutDelimiter.split(delimiter);
+
         int sum = 0;
         for (String num : nums) {
-            sum += Integer.parseInt(num);
+            sum += Integer.parseInt(num.trim());
         }
 
         return sum;
