02704ed082c3f62483d4610754429dde7c1a17e7
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-07-01 16:17:17 +0530

    Add `@BeforeAll` annotation to make testcases simple

--- app/src/test/java/stringcalculator/StringCalculatorTest.java
+++ app/src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,50 +1,50 @@
 package stringcalculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    private static StringCalculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        calculator = new StringCalculator();
+    }
+
     @Test
-    void Add_EmptyString_ReturnsZero () {
-        StringCalculator calculator = new StringCalculator();
+    void Add_EmptyString_ReturnsZero() {
         int result = calculator.add("");
         assertEquals(0, result);
     }
 
     @Test
     void testAdd_SingleNumber_ReturnsNumber() {
-        StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1");
         assertEquals(1, result);
     }
 
     @Test
     void testAdd_TwoNumbers_ReturnsSum() {
-        StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1,5");
         assertEquals(6, result);
     }
 
     @Test
     void testAdd_NewLinesBetweenNumbers_ReturnsSum() {
-        StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1\n2,3");
         assertEquals(6, result);
     }
 
     @Test
     void testAdd_CustomDelimiter_ReturnsSum() {
-        StringCalculator calculator = new StringCalculator();
         int result = calculator.add("//;\n1;2");
         assertEquals(3, result);
     }
 
     @Test
     void testAdd_NegativeNumber_ThrowsException() {
-        StringCalculator calculator = new StringCalculator();
-
         try {
             calculator.add("1,-2,3");
             fail("Expected IllegalArgumentException was not thrown");
